9b2f6dffc29e599390bbf8777ed7d25b3a8a618d
dcts <thomas@liist.com>
2022-04-01 02:34:58 +0200

    :sparkle: add ticker V1 and :rocket: deploy

--- src/components/home/_Home.js
+++ src/components/home/_Home.js
@@ -5,8 +5,7 @@ const Home = () => {
     <div className="home-container">
       <h1 className="main-title">Create Beautiful <br />NFT Banners</h1>
       <p className="description">
-        <span>Wagmibanners helps you </span><strong>auto-generate</strong> <span>beautiful banners</span><br />
-        <span>to showcase your</span> <strong>Deadfellaz</strong><span>,</span> <strong>Coolcats</strong> <span>or</span> <strong>Loosers</strong> <span>NFTs on twitter.</span>
+        <span>Wagmibanners helps you </span><strong>auto-generate</strong> <span>beautiful banners to showcase your</span> <strong>Deadfellaz</strong><span>,</span> <strong>Coolcats</strong> <span>or</span> <strong>Loosers</strong> <span>NFTs on twitter.</span>
       </p>
       <Ticker tickerContent={"ticker1"}/>
       <Ticker tickerContent={"ticker2"}/>
